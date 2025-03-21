c10a336ae5e6fb54f8099528c87453192c65cf4b
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-21 15:08:49 -0700

    Update DiseaseList.js

--- pandemix/src/components/DiseaseList.js
+++ pandemix/src/components/DiseaseList.js
@@ -1,40 +1,19 @@
-// /src/pages/HomePage.js
-import React, { useContext, useState } from 'react';
-import Header from '../components/Header';
-import SearchBar from '../components/SearchBar';
-import DiseaseList from '../components/DiseaseList';
-import DiseaseModal from '../components/DiseaseModal';
-import { DiseaseContext } from '../context/DiseaseContext';
-import useDiseaseData from '../hooks/useDiseaseData';
-import { fetchDiseaseDetails } from '../api/diseaseApi';
+import React from 'react';
+import DiseaseCard from './DiseaseCard';
+import '../styles/DiseaseList.css';
 
-const HomePage = () => {
-  const { diseases, setDiseases } = useContext(DiseaseContext);
-  const [searchQuery, setSearchQuery] = useState('');
-  const { data, loading, error } = useDiseaseData(searchQuery);
-  const [selectedDisease, setSelectedDisease] = useState(null);
-
-  React.useEffect(() => {
-    if (data) setDiseases(data);
-  }, [data, setDiseases]);
-
-  const handleCardClick = async (disease) => {
-    const details = await fetchDiseaseDetails(disease.id);
-    setSelectedDisease(details);
-  };
-
-  const closeModal = () => setSelectedDisease(null);
-
+const DiseaseList = ({ diseases }) => {
   return (
-    <div className="home-page">
-      <Header />
-      <SearchBar onSearch={setSearchQuery} />
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      <DiseaseList diseases={diseases} onClick={handleCardClick} />
-      {selectedDisease && <DiseaseModal disease={selectedDisease} onClose={closeModal} />}
+    <div className="disease-list">
+      {diseases.length > 0 ? (
+        diseases.map((disease) => (
+          <DiseaseCard key={disease.id} disease={disease} />
+        ))
+      ) : (
+        <p>No diseases found.</p>
+      )}
     </div>
   );
 };
 
-export default HomePage;
+export default DiseaseList;
