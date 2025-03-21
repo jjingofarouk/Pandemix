6895f8693f320d28520aec341e3aafafafeb9f8f
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-21 14:33:29 -0700

    Update HomePage.js

--- pandemix/src/pages/HomePage.js
+++ pandemix/src/pages/HomePage.js
@@ -1,18 +1,29 @@
-import React, { useContext } from 'react';
+// /src/pages/HomePage.js
+import React, { useContext, useState } from 'react';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import DiseaseList from '../components/DiseaseList';
+import DiseaseModal from '../components/DiseaseModal';
 import { DiseaseContext } from '../context/DiseaseContext';
 import useDiseaseData from '../hooks/useDiseaseData';
+import { fetchDiseaseDetails } from '../api/diseaseApi';
 
 const HomePage = () => {
   const { diseases, setDiseases } = useContext(DiseaseContext);
-  const [searchQuery, setSearchQuery] = React.useState('');
+  const [searchQuery, setSearchQuery] = useState('');
   const { data, loading, error } = useDiseaseData(searchQuery);
+  const [selectedDisease, setSelectedDisease] = useState(null);
 
   React.useEffect(() => {
     if (data) setDiseases(data);
   }, [data, setDiseases]);
+
+  const handleCardClick = async (disease) => {
+    const details = await fetchDiseaseDetails(disease.id);
+    setSelectedDisease(details);
+  };
+
+  const closeModal = () => setSelectedDisease(null);
 
   return (
     <div className="home-page">
@@ -20,7 +31,8 @@ const HomePage = () => {
       <SearchBar onSearch={setSearchQuery} />
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
-      <DiseaseList diseases={diseases} />
+      <DiseaseList diseases={diseases} onClick={handleCardClick} />
+      {selectedDisease && <DiseaseModal disease={selectedDisease} onClose={closeModal} />}
     </div>
   );
 };
