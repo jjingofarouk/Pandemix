de86f437b48ec89cbeb1cdc877a86602cf42676c
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-21 14:19:16 -0700

    Update index.js

--- pandemix/src/index.js
+++ pandemix/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { DiseaseProvider } from './context/DiseaseContext';
 import './styles/App.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <DiseaseProvider>
       <App />
     </DiseaseProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
